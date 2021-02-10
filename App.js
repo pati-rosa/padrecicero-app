9efb4909074b2d663be92630cd7f63efeab4f2d9
patirosa95 <patirosa995@gmail.com>
2021-02-10 11:39:43 -0300

    feat: react-navigation/stack installed

--- App.js
+++ App.js
@@ -6,15 +6,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/components/main';
 import { StoreProvider } from 'easy-peasy';
 import store from './src/stores/store';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StoreProvider store={store} >
-        <Main />
-      </StoreProvider>
-    </View>
-  );
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StoreProvider store={store} >
+          <Main />
+        </StoreProvider>
+      </View>
+    </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
