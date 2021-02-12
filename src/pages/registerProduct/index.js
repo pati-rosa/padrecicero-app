956a952f4b8c5a6a8571277dc5947dde9962ff50
patirosa95 <patirosa995@gmail.com>
2021-02-11 22:37:28 -0300

    feat: text input from registerProduct created

--- src/pages/registerProduct/index.js
+++ src/pages/registerProduct/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, TextInput,Text, StyleSheet } from 'react-native';
 
 import TabTitle from '../../components/tabTitle';
 import SelectCategory from '../../components/selectCategory';
 
 export default function RegisterProduct() {
+    const [text, setText] = useState('');
+    console.log(text)
     return(
+        
         <View>
-                <TabTitle title="CADASTRAR PRODUTO" />
+            <TabTitle title="CADASTRAR PRODUTO" />
             <View style = {{ backgroundColor: 'red', padding: 20, margin:20}}>
                 <SelectCategory />
             </View>
@@ -15,9 +18,13 @@ export default function RegisterProduct() {
                 <Text>Selecionar nome do produto</Text>   
                 <TextInput  
                 placeholder="Escolher nome do produto"
-                placeholderTextColor="#A9A9A9" 
+                placeholderTextColor="#A9A9A9"
+                onChangeText={text => setText(text)}
+                defaultValue={text}
+
                 />
             </View>
         </View>
     );
+    
 }
