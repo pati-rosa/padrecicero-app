b0d07e106947f3b90c7eb673e99d1ea7a5a92025
patirosa95 <patirosa995@gmail.com>
2021-02-11 21:02:51 -0300

    feat: select category finished

--- src/components/selectCategory/index.js
+++ src/components/selectCategory/index.js
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
-import { Picker } from '@react-native-picker/picker';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Picker, pickerSelectStyles } from '@react-native-picker/picker';
+import axios from 'axios';
+
+import { apiUrl } from '../../services';
+
 
 export default function SelectCategory() {
-    const [state, setState] = useState('java');
+    const [selectedCategory, setSelectedCategory] = useState();
+    const [categories, setCategories] = useState([]);
+
+    console.log('SELECTED CATEGORY: ', selectedCategory);
+
+    useEffect(() => {
+        console.log('CARREGUEI DA API!');
+
+        axios.get(`${apiUrl}/category/`).then(response => {
+            setCategories(response.data.categories);
+        })
+    }, []);
+
+
     return (
-        <Picker
-            selectedValue={state}
-            style={{ height: 50, width: 100 }}
-            onValueChange={(itemValue) =>
-                setState(itemValue)
-            }>
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript" value="js" />
-        </Picker>
+        <View>
+            <Picker
+                selectedValue={selectedCategory}
+                style={{ width: 300, color: "#1A110F" }}
+                onValueChange={(itemValue) =>
+                    setSelectedCategory(itemValue)
+                }>
+                {categories.map((category) => <Picker.Item label={category.name} value={category} key={category._id} />)}
+                {/* <Picker.Item label="Java" value="java" /> */}
+            </Picker>
+        </View>
     );
 };
+
+
